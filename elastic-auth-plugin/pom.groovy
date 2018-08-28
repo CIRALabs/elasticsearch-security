7b93673e1656508643f713067f14d11435c4ab66
Zachary Wilkins <zachary.wilkins@cira.ca>
2018-08-28 17:02:51 -0400

    Version bump elastic auth for 6.4

--- elastic-auth-plugin/pom.groovy
+++ elastic-auth-plugin/pom.groovy
@@ -2,7 +2,7 @@ project {
   modelVersion '4.0.0'
   groupId 'ca.ciralabs'
   artifactId 'elastic-auth-plugin'
-  version '1.0-SNAPSHOT'
+  version '1.1'
   description 'Provides authentication support for Elasticsearch'
   properties {
     'project.build.sourceEncoding' 'UTF-8'
@@ -19,7 +19,7 @@ project {
     dependency {
       groupId 'org.elasticsearch'
       artifactId 'elasticsearch'
-      version '6.3.0'
+      version '6.4.0'
       scope 'provided'
     }
     dependency {
