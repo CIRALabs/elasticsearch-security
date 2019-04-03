c2ac6c11fd846c7077fc1e863a81de89e498c303
Lucas Estienne <lucas.estienne@cira.ca>
2019-04-03 12:43:39 -0400

    update to 7.0.0

--- elastic-auth-plugin/pom.groovy
+++ elastic-auth-plugin/pom.groovy
@@ -2,7 +2,7 @@ project {
   modelVersion '4.0.0'
   groupId 'ca.ciralabs'
   artifactId 'elastic-auth-plugin'
-  version '2019.03.30'
+  version '2019.04.03'
   description 'Provides authentication support for Elasticsearch'
   properties {
     'project.build.sourceEncoding' 'UTF-8'
@@ -19,7 +19,7 @@ project {
     dependency {
       groupId 'org.elasticsearch'
       artifactId 'elasticsearch'
-      version '6.7.0'
+      version '7.0.0-rc1'
       scope 'provided'
     }
     dependency {
